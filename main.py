9284d2186b648c4c9017b9c134be451ad5ac53e2
isroil <312@.ru>
2025-07-11 01:48:00 +0500

    nifiga ya rad ochen

--- main.py
+++ main.py
@@ -31,7 +31,9 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=["http://localhost:5173",       # для локальной разработки
+        "https://store-ma.netlify.app",         # для размещения на netlify
+        ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
